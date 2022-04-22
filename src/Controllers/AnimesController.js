ac31024f0060c0c798582e8208537fbf24c4dc97
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-22 00:58:12 -0300

    Implementado os métodos de listar, deletar e atualizar anime por id.

--- src/Controllers/AnimesController.js
+++ src/Controllers/AnimesController.js
@@ -6,6 +6,18 @@ class AnimesController {
       res.status(200).send(animes);
     });
   };
+  static ListarAnimesPorId = (req, res) => {
+    const id = req.params.id;
+    animes.findById(id, (err, animes) => {
+      if (err) {
+        res
+          .status(400)
+          .send({ message: `${err.message} - id do livro não localizado` });
+      } else {
+        res.status(200).send(animes);
+      }
+    });
+  };
   static CadastrarAnime = (req, res) => {
     let cadastroAnime = new animes(req.body);
     cadastroAnime.save((err) => {
@@ -15,6 +27,26 @@ class AnimesController {
           .send({ message: `${err.message} - falha ao cadastrar anime` });
       } else {
         res.status(201).send(cadastroAnime.toJSON());
+      }
+    });
+  };
+  static AtualizarAnime = (req, res) => {
+    const id = req.params.id;
+    animes.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+      if (!err) {
+        res.status(200).send({ message: "Anime atualizado com sucesso" });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+  static ExcluirAnime = (req, res) => {
+    const id = req.params.id;
+    animes.findByIdAndDelete(id, (err) => {
+      if (!err) {
+        res.status(200).send({ message: "livro removido com sucesso" });
+      } else {
+        res.status(500).send({ message: err.message });
       }
     });
   };

dd69e964bc789f8763a52ee9b695393f2c44ddac
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-22 00:18:14 -0300

    importado a model, routes e configuraçoes com banco de dados,

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { json } from "express/lib/response";
+import db from "./Config/dbConnect.js";
+import animes from "./Models/AnimesModel.js";
+import routes from "./Routes/index.js";
 
+db.on("error", console.log.bind(console, "Erro de conexão"));
+db.once("open", () => {
+  console.log("conexão com banco feita com sucesso");
+});
 const app = express();
 
+// const anime = [{ id: 1, titulo: "One piece" }];
+
 app.use(express.json());
+routes(app);
 
 export default app;

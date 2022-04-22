77f30ec4de6934bfe8c4dc04a835e9b99d4a3209
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-22 00:58:27 -0300

    retirado a importação da model

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./Config/dbConnect.js";
-import animes from "./Models/AnimesModel.js";
 import routes from "./Routes/index.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));

ecec377456e3b93fa0d609d5b881798af143bd23
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-22 00:18:48 -0300

    Configurado o server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import app from "./app";
+import app from "./app.js";
 
-const porta = 3000;
+const porta = process.env.PORT || 3000;
 
 app.listen(porta, () => {
-  console.log(`servidor aberto na http//:localhost:${porta}/`);
+  console.log(`servidor aberto na http://localhost:${porta}/`);
 });
